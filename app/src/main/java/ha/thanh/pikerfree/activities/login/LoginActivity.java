79592ed7f12970ed00810f5420e157eb0f5bf86a
THANH_PC\HaVan <thanhahvan1@gmail.com>
2017-08-28 21:38:07 +0700

    add auth, login, registation with email on firebase.

--- app/src/main/java/ha/thanh/pikerfree/activities/login/LoginActivity.java
+++ app/src/main/java/ha/thanh/pikerfree/activities/login/LoginActivity.java
@@ -1,15 +1,104 @@
 package ha.thanh.pikerfree.activities.login;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import ha.thanh.pikerfree.R;
+import ha.thanh.pikerfree.activities.mainActivity.MainActivity;
+import ha.thanh.pikerfree.activities.signup.SignUpActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+
+    @BindView(R.id.et_username)
+    EditText etUserName;
+    @BindView(R.id.et_pass)
+    EditText etPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        auth = FirebaseAuth.getInstance();
+        checkLogIn();
+    }
+
+    public void checkLogIn() {
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    @OnClick(R.id.btn_log_up)
+    public void doSignUp() {
+        startActivity(new Intent(this, SignUpActivity.class));
+    }
+
+    @OnClick(R.id.btn_forget)
+    public void doForgetPassword() {
+        // start activity of forget password.
+    }
+
+    @OnClick(R.id.btn_log_in)
+    public void dpLogIn() {
+
+        String email = etUserName.getText().toString();
+        final String password = etPass.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //authenticate user
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            // there was an error
+                            if (password.length() < 6) {
+                                etPass.setError(getString(R.string.minimum_password));
+                            } else {
+                                Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof FirebaseAuthException) {
+                    ((FirebaseAuthException) e).getErrorCode();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/ha/thanh/pikerfree/activities/signup/SignUpActivity.java
+++ app/src/main/java/ha/thanh/pikerfree/activities/signup/SignUpActivity.java
@@ -1,15 +1,80 @@
 package ha.thanh.pikerfree.activities.signup;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import ha.thanh.pikerfree.R;
+import ha.thanh.pikerfree.activities.mainActivity.MainActivity;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    @BindView(R.id.et_email)
+    EditText etEmail;
+    @BindView(R.id.et_username)
+    EditText etUserName;
+    @BindView(R.id.et_pass)
+    EditText etPass;
+    @BindView(R.id.et_pass_confirm)
+    EditText etPassConfirm;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+        auth = FirebaseAuth.getInstance();
+    }
+
+    @OnClick(R.id.btn_sign_up)
+    public void doSignUp() {
+
+        String email = etEmail.getText().toString().trim();
+        String password = etPass.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (password.length() < 6) {
+            Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //create user
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(SignUpActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(SignUpActivity.this, "Authentication failed." + task.getException(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+                            finish();
+                        }
+                    }
+                });
+
     }
 }
